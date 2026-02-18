9597873cb8a62364aa5ecccb7ae24485c99d29af
Matheus Cruz <developer.matheuscruz@gmail.com>
2026-02-18 00:55:06 -0300

    feat: poo project built

---
+++ java - poo/src/br/com/dvlprmatheus/desafio/dominio/Course.java
@@ -0,0 +1,30 @@
+package br.com.dvlprmatheus.desafio.dominio;
+
+public class Course extends Content {
+    private int workload;
+
+    @Override
+    public double calculateXp() {
+        return XP_DEFAULT * workload;
+    }
+
+    public Course() {
+    }
+
+    public int getWorkload() {
+        return workload;
+    }
+
+    public void setWorkload(int workload) {
+        this.workload = workload;
+    }
+
+    @Override
+    public String toString() {
+        return "Course{" +
+                "title='" + getTitle() + '\'' +
+                ", description='" + getDescription() + '\'' +
+                ", workload=" + workload +
+                '}';
+    }
+}
