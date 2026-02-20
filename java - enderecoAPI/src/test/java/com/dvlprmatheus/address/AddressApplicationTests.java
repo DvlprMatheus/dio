2259f564564f03700a871dc414b34add4cb404f1
Matheus Cruz <developer.matheuscruz@gmail.com>
2026-02-19 23:58:43 -0300

    feat: Initial project

---
+++ java - enderecoAPI/src/test/java/com/dvlprmatheus/address/AddressApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dvlprmatheus.address;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AddressApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
