2a71a66bec323d90c86428e52ed75ef9e65efa51
Matheus Cruz <developer.matheuscruz@gmail.com>
2026-02-20 00:40:00 -0300

    feat: add ViaCep implementation

---
+++ java - enderecoAPI/src/main/java/com/dvlprmatheus/address/service/impl/ClienteServiceImpl.java
@@ -0,0 +1,60 @@
+package com.dvlprmatheus.address.service.impl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import java.util.Optional;
+import com.dvlprmatheus.address.model.Cliente;
+import com.dvlprmatheus.address.model.Endereco;
+import com.dvlprmatheus.address.repository.ClienteRepository;
+import com.dvlprmatheus.address.repository.EnderecoRepository;
+import com.dvlprmatheus.address.service.ClienteService;
+import com.dvlprmatheus.address.service.ViaCepService;
+
+public class ClienteServiceImpl implements ClienteService {
+    
+	@Autowired
+	private ClienteRepository clienteRepository;
+	@Autowired
+	private EnderecoRepository enderecoRepository;
+	@Autowired
+	private ViaCepService viaCepService;
+	
+	@Override
+	public Iterable<Cliente> buscarTodos() {
+		return clienteRepository.findAll();
+	}
+
+	@Override
+	public Cliente buscarPorId(Long id) {
+		Optional<Cliente> cliente = clienteRepository.findById(id);
+		return cliente.get();
+	}
+
+	@Override
+	public void inserir(Cliente cliente) {
+		salvarClienteComCep(cliente);
+	}
+
+	@Override
+	public void atualizar(Long id, Cliente cliente) {
+		Optional<Cliente> clienteBd = clienteRepository.findById(id);
+		if (clienteBd.isPresent()) {
+			salvarClienteComCep(cliente);
+		}
+	}
+
+	@Override
+	public void deletar(Long id) {
+		clienteRepository.deleteById(id);
+	}
+
+	private void salvarClienteComCep(Cliente cliente) {
+		String cep = cliente.getEndereco().getCep();
+		Endereco endereco = enderecoRepository.findById(cep).orElseGet(() -> {
+			Endereco novoEndereco = viaCepService.consultarCep(cep);
+			enderecoRepository.save(novoEndereco);
+			return novoEndereco;
+		});
+		cliente.setEndereco(endereco);
+		clienteRepository.save(cliente);
+	}
+}
